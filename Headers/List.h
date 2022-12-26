12a0e478b44672b7e26c8ab1fcb15a129892c564
pcRipper <maximuspro100@gmail.com>
2022-12-26 21:19:56 +0200

    minor changes

--- Headers/List.h
+++ Headers/List.h
@@ -57,3 +57,361 @@ public:
 	T* operator[](size_t index);
 	~List();
 };
+
+template <class T>
+ListNode<T>::ListNode(T* element, ListNode* next) :
+	element(element),
+	next(next)
+{}
+
+template<class T>
+ListNode<T>::ListNode(T& element, ListNode* next) :
+	element(new T(element)),
+	next(next)
+{};
+
+template <class T>
+ListNode<T>::ListNode(const ListNode<T>& obj) {
+	element = new T(*obj.element);
+	next = nullptr;
+}
+
+template <class T>
+ostream& operator<<(ostream& out, const ListNode<T>* element) {
+	out << *element->element;
+	//cout << "[" << &*element->element << "]" << *element->element;
+	return out;
+}
+
+template <class T>
+ostream& operator<<(ostream& out, const ListNode<T>& element)
+{
+	out << element->element;
+	return out;
+}
+
+template <class T>
+ListNode<T>* ListNode<T>::operator++(int) {
+	return next;
+}
+
+template <class T>
+ListNode<T>::~ListNode() {
+	delete element;
+	next = nullptr;
+}
+
+template <class T>
+List<T>::List() {
+	size = 0;
+	head = nullptr;
+	tail = nullptr;
+	current = nullptr;
+}
+
+
+template <class T>
+List<T>::List(const List<T>& obj) {
+	if (obj.head != nullptr) {
+		ListNode<T>* current_obj;
+
+		head = new ListNode<T>(*obj.head);
+		current = head;
+		current_obj = obj.head->next;
+
+		while (current_obj != nullptr) {
+
+			current->next = new ListNode<T>(*current_obj);
+
+			current = (*current)++;
+			current_obj = current_obj->next;
+		}
+
+		size = obj.size;
+		tail = current;
+		current = nullptr;
+	}
+	else {
+		List();
+	}
+}
+
+template <class T>
+List<T>::List(T* arguments, size_t size) {
+	for (int k = 0; k < size; k++) {
+		add(arguments[k]);
+	}
+}
+
+template <class T>
+void List<T>::add(T* element) {
+	if (head == nullptr) {
+		tail = head = new ListNode<T>(element);
+	}
+	else {
+		tail = tail->next = new ListNode<T>(element);
+	}
+	size++;
+}
+
+template <class T>
+void List<T>::add(T& element) {
+	if (head == nullptr) {
+		tail = head = new ListNode<T>(element);
+	}
+	else {
+		tail = tail->next = new ListNode<T>(element);
+	}
+	size++;
+}
+
+template <class T>
+void List<T>::addOnSorted(T* element, bool (*comparer)(T&, T&)) {
+
+	if (head != nullptr) {
+		if (comparer(*element, *head->element)) {
+			head = new ListNode<T>(element, head);
+		}
+		else {
+			for (current = head->next, previous = head; comparer(*current->element, *element) && current != nullptr;) {
+				previous = current;
+				current = (*current)++;
+			}
+			previous->next = new ListNode<T>(element, current);
+
+			if (current == nullptr)tail = previous->next;
+		}
+	}
+	else {
+		add(element);
+	}
+
+	size++;
+
+}
+
+template <class T>
+void List<T>::concat(List<T>* list) {
+	for (current = list->head; current != nullptr; current = (*current)++)add(new T(current->element));
+}
+
+template<class T>
+void List<T>::remove(size_t index) {
+	if (head != nullptr) {
+		size_t element = 0;
+
+		for (current = head; element < index && current != nullptr; element++) {
+			previous = current;
+			current = (*current)++;
+		}
+
+		if (element == index && current != nullptr) {
+
+			if (current == head) {
+				head = current->next;
+			}
+			else if (current->next == nullptr) {
+				tail = previous;
+				previous->next = nullptr;
+			}
+			else {
+				previous->next = current->next;
+			}
+
+			current->next = nullptr;
+			delete current;
+			size--;
+
+		}
+	}
+}
+
+template <class T>
+void List<T>::reverse() {
+	if (head != nullptr) {
+		ListNode<T>* prev = nullptr;
+		current = head->next;
+		tail = head;
+
+		while (true) {
+			head->next = prev;
+			prev = head;
+			if (current == nullptr)break;
+			head = current;
+			current = (*current)++;
+		}
+	}
+}
+
+template <class T>
+ListNode<T>* List<T>::find(bool(*comparer)(T&, T&), T* element) {
+	for (current = head; current != nullptr && !comparer(*current->element, *element); current = (*current)++);
+
+	return current;
+}
+
+template <class T>
+void List<T>::setTail() {
+	for (current = head; current->next != nullptr; current = (*current)++);
+	tail = current;
+}
+
+template <class T>
+List<T>* List<T>::filter(bool (*func)(T&)) {
+	List<T>* result = new List<T>();
+
+	for (current = head; current != nullptr; current = (*current)++) {
+		if (func(*current->element))result->add(current->element);
+	}
+
+	return result;
+}
+
+template <class T>
+void List<T>::mapIn(T* (*func)(T&)) {
+	for (current = head; current != nullptr; current = (*current)++) {
+		T* prev_element = current->element;
+
+		current->element = func(*current->element);
+
+		delete prev_element;
+	}
+}
+
+template <class T>
+template <class F>
+List<F>* List<T>::mapOn(F* (*func)(T&)) {
+	List<F>* result = new List<F>();
+
+	for (current = head; current != nullptr; current = (*current)++) {
+		result->add(func(*current->element));
+	}
+
+	return result;
+}
+
+template<class T>
+template<class F>
+F* List<T>::fold(void (*fold_function)(F*, T*), F* start_value)
+{
+	for (current = head; current != nullptr; current = (*current)++) {
+		fold_function(start_value, current->element);
+	}
+
+	return start_value;
+}
+
+template<class T>
+void List<T>::show(string separator, string begin, string end) {
+	if (head != nullptr) {
+		cout << begin;
+		for (current = head; current != nullptr; current = (*current)++) {
+			cout << current << separator;
+		}
+		cout << end;
+	}
+	else cout << "\nList is empty!\n";
+}
+
+template<class T>
+void List<T>::show_t1()
+{
+	show(", ", to_string(size) + " : ", "\b\b;\n");
+}
+
+template<class T>
+void List<T>::show_t2()
+{
+	show("\n\t", to_string(size) + " :\n\t", "\r;\n");
+}
+
+template<class T>
+void List<T>::pickSort(bool (*comparer)(T&, T&)) {
+	List<T>* new_list = new List<T>();
+
+	while (head != nullptr) {
+
+		size_t k = 1;
+		size_t max = 0;
+		ListNode<T>* maxP = head;
+
+		for (current = head->next; current != nullptr; current = (*current)++) {
+			if (comparer(*current->element, *maxP->element)) {
+				max = k;
+				maxP = current;
+			}
+			k++;
+		}
+
+		new_list->add(new T(*maxP->element));
+		remove(max);
+	}
+
+	head = new_list->head;
+	tail = new_list->tail;
+	size = new_list->size;
+
+}
+
+template<class T>
+void List<T>::insertSort(bool (*comparer)(T&, T&)) {
+	if (head != nullptr) {
+
+		ListNode<T>* prev = head, * picked, * current;
+
+		for (current = head->next; current != nullptr; current = (*current)++) {
+
+			if (comparer(*current->element, *prev->element)) {
+
+				picked = current;
+				prev->next = current->next;
+
+				if (comparer(*picked->element, *head->element)) {
+					picked->next = head;
+					head = picked;
+				}
+				else {
+					for (current = head; current->next != nullptr && !comparer(*picked->element, *current->next->element); current = (*current)++);
+
+					picked->next = current->next;
+					current->next = picked;
+				}
+
+				current = prev;
+			}
+			prev = current;
+
+		}
+
+		setTail();
+
+	}
+}
+
+template <class T>
+void List<T>::clean() {
+
+	if (head != nullptr) {
+		for (current = head->next; true;) {
+			delete head;
+			if (current == nullptr)return;
+			head = current;
+			current = head->next;
+		}
+	}
+
+	current = head = tail = nullptr;
+}
+
+template<class T>
+T* List<T>::operator[](size_t index) {
+	for (current = head; index != 0 && current != nullptr; current = (*current)++) {
+		index--;
+	}
+	return((current == nullptr) ? nullptr : current->element);
+}
+
+template<class T>
+List<T>::~List() {
+	clean();
+}
--- Headers/Tree.h
+++ Headers/Tree.h
@@ -1,8 +1,10 @@
 #pragma once
-
 #include <iostream>
-#include "List.cpp"
+#include <functional>
+#include "List.h"
 using namespace std;
+
+#pragma region Node<T>
 
 template <class T>
 class Node {
@@ -20,6 +22,16 @@ public:
         left(left),
         right(right)
     {};
+    Node(
+        T& element,
+        Node<T>* prev = nullptr,
+        Node<T>* left = nullptr,
+        Node<T>* right = nullptr) :
+        element(new T(element)),
+        prev(prev),
+        left(left),
+        right(right)
+    {};
     Node(Node<T>* obj);
     Node(Node<T> const& obj);
     template <class T> friend std::ostream& operator << (std::ostream& out, Node<T>* element);
@@ -27,6 +39,55 @@ public:
     bool operator == (Node<T> const& element);
     ~Node();
 };
+
+template<class T>
+Node<T>::Node(Node<T>* obj) {
+    if (obj != nullptr) {
+        element = new T(*obj->element);
+        left = obj->left;
+        right = obj->right;
+        prev = obj->prev;
+        /*       left = prev = right = nullptr;*/
+    }
+}
+
+template<class T>
+Node<T>::Node(Node<T> const& obj) {
+    element = new T(obj.element);
+    left = obj.left;
+    right = obj.right;
+    prev = obj.prev;
+    //left = right = prev = nullptr;
+}
+
+template <class T>
+std::ostream& operator << (std::ostream& out, Node<T>* element) {
+    if (element == nullptr || element->element == nullptr)out << "__";
+    else out << *(element->element);
+    return(out);
+}
+
+template <class T>
+std::ostream& operator << (std::ostream& out, Node<T>& element) {
+    if (element.element == nullptr)out << "__";
+    else out << *element.element;
+    return(out);
+}
+
+template <class T>
+bool Node<T>::operator==(Node<T> const& element) {
+    return *this->element == *element.element;
+}
+
+template <class T>
+Node<T>::~Node() {
+    prev = left = right = nullptr;
+    delete element;
+}
+
+#pragma endregion
+
+#pragma region NWP<T>
 
 template <class T>
 struct NWP {
@@ -38,6 +99,10 @@ struct NWP {
     }
 };
 
+#pragma endregion
+
+#pragma region Tree<T>
+
 template <class T>
 class Tree {
 protected:
@@ -46,32 +111,362 @@ public:
     Tree();
     Node<T>* getRoot();
     void setRoot(Node<T>* root);
-    virtual void add(T* element) = 0;
-    virtual void remove(bool (*predicate)(T*), Node<T>* (*normalization)(Node<T>*), bool recursively = false) = 0;
-    int countNodes(Node<T>* current = root);
-    int countLeafes(Node<T>* current = root);
-    virtual List<T> findAll(bool (*predicate)(T*));
-    //in passed comparer MUST be defined cases for `NULL` values
-    virtual T* getMost(bool (*comparer)(T*, T*));
-    template <class F> F* fold(F* (*fold_function)(F*, T*), F* start_value);
-    void showTree();
+    virtual string add(T* element) = 0;
+    virtual string add(T& element) = 0;
+    virtual void remove(function<bool(T*)> predicate, function<Node<T>* (Node<T>*)> normalization, bool recursively = false) = 0;
+    int countNodes(Node<T>* current);
+    int countLeafes(Node<T>* current);
+    int countHeight(Node<T>* current, int height = 0);
+    List<T>* findAll(function<bool(T*)> predicate);
+    virtual List<T>* toList();
+    T* getMost(bool (*comparer)(T*, T*));
+    template <class F> F* fold(void (*fold_function)(F*, T*), F* start_value);
+    virtual void show();
+    virtual void show_pointers();
 protected:
-    virtual void findAllHelper(bool (*predicate)(T*), Node<T>* current, List<T>* result);
-    virtual void show(Node<T>* current, int level = 0);
-    virtual void getMostHelper(bool (*comparer)(T*, T*), Node<T>* current, T* most);
-    template <class F> F* foldHelper(F* (*fold_function)(F*, T*), F* value, Node<T>* current);
+    void findAllHelper(function<bool(T*)> predicate, Node<T>* current, List<T>* result);
+    virtual void showTree(Node<T>* current, int level = 0);
+    virtual void showTreePointers(Node<T>* current, List<int> levels, int level = 0);
+    virtual void getMostHelper(bool (*comparer)(T*, T*), Node<T>* current, T*& most);
+    virtual void getAllHelper(Node<T>* current, List<T>*& list);
+    template <class F> void foldHelper(void (*fold_function)(F*, T*), F* value, Node<T>* current);
 public:
     ~Tree();
 };
 
 template <class T>
-class SortedTree : public Tree<T> {
+Tree<T>::Tree() :root(nullptr), current(nullptr), previous(nullptr) {};
+
+template <class T>
+Tree<T>::~Tree() {}
+
+template <class T>
+Node<T>* Tree<T>::getRoot() {
+    return root;
+}
+
+template <class T>
+void Tree<T>::setRoot(Node<T>* root) {
+    if (root != nullptr) {
+        this->root = new Node<T>(*root);
+    }
+}
+
+template<class T>
+int Tree<T>::countNodes(Node<T>* current)
+{
+    if (current != nullptr) {
+        return 1 + countNodes(current->left) + countNodes(current->right);
+    }
+    return 0;
+}
+
+template<class T>
+int Tree<T>::countLeafes(Node<T>* current)
+{
+    if (current != nullptr) {
+        if (current->left == nullptr && current->right == nullptr)return 1;
+        return countLeafes(current->left) + countLeafes(current->right);
+    }
+    return 0;
+}
+
+template<class T>
+int Tree<T>::countHeight(Node<T>* current, int height)
+{
+    if (current != nullptr) {
+        return max(height + 1, max(countHeight(current->left, height + 1), countHeight(current->right, height + 1)));
+    }
+    return height;
+}
+
+template<class T>
+List<T>* Tree<T>::findAll(function<bool(T*)> predicate)
+{
+    List<T>* result = new List<T>();
+
+    findAllHelper(predicate, root, result);
+
+    return result;
+}
+
+template<class T>
+List<T>* Tree<T>::toList()
+{
+    List<T>* result = new List<T>();
+    getAllHelper(root, result);
+    return result;
+}
+
+template<class T>
+T* Tree<T>::getMost(bool(*comparer)(T*, T*))
+{
+    T* result = nullptr;
+    getMostHelper(comparer, root, result);
+    //if (result != nullptr) {
+    //    result = new T(result);
+    //}
+    return result;
+}
+
+template<class T>
+template<class F>
+inline F* Tree<T>::fold(void (*fold_function)(F*, T*), F* start_value)
+{
+    foldHelper(fold_function, start_value, root);
+    return start_value;
+}
+
+template<class T>
+template<class F>
+void Tree<T>::foldHelper(void (*fold_function)(F*, T*), F* value, Node<T>* current)
+{
+    if (current != nullptr) {
+        fold_function(value, current->element);
+        foldHelper(fold_function, value, current->left);
+        foldHelper(fold_function, value, current->right);
+    }
+}
+
+template<class T>
+void Tree<T>::getMostHelper(bool (*comparer)(T*, T*), Node<T>* current, T*& most)
+{
+    if (current != nullptr) {
+        if (most == nullptr)most = current->element;
+        else if (current->element != nullptr) {
+            if (comparer(most, current->element))most = current->element;
+        }
+        getMostHelper(comparer, current->left, most);
+        getMostHelper(comparer, current->right, most);
+    }
+}
+
+template<class T>
+void Tree<T>::getAllHelper(Node<T>* current, List<T>*& list)
+{
+    if (current != nullptr) {
+        list->add(*current->element);
+        getAllHelper(current->left, list);
+        getAllHelper(current->right, list);
+    }
+}
+
+template<class T>
+void Tree<T>::findAllHelper(function<bool(T*)> predicate, Node<T>* current, List<T>* result)
+{
+    if (current != nullptr) {
+        if (predicate(current->element)) {
+            result->add(current->element);
+        }
+
+        findAllHelper(predicate, current->left, result);
+        findAllHelper(predicate, current->right, result);
+    }
+}
+
+template <class T>
+void Tree<T>::show() {
+    this->showTree(root, 0);
+}
+
+template<class T>
+void Tree<T>::show_pointers()
+{
+    this->showTreePointers(root, *new List<int>(), 0);
+}
+
+template <class T>
+void Tree<T>::showTree(Node<T>* current, int level) {
+    if (current != nullptr) {
+        showTree(current->right, level + 1);
+        //std::cout << std::string(level * 3, ' ') << level + 1 << "|" << current << "\n\n";
+        cout << std::string(level * 3, ' ');
+        if (current != root)cout << "\b\b" << ((current->prev->right == current) ? char(218) : char(192)) << ">";
+        std::cout << level + 1 << "|" << current << "\n";
+        showTree(current->left, level + 1);
+    }
+}
+
+template<class T>
+void Tree<T>::showTreePointers(Node<T>* current, List<int> levels, int level)
+{
+    if (current != nullptr) {
+
+
+        showTreePointers(current->right, List<int>(), level + 1);
+
+        cout << std::string(level * 3, ' ');
+        if (current != root)cout << "\b\b" << ((current->prev->right == current) ? char(218) : char(192)) << ">";
+        std::cout << level + 1 << "|" << current << "\n";
+
+        showTreePointers(current->left, List<int>(), level + 1);
+
+    }
+}
+
+#pragma endregion
+
+#pragma region SortedTree<T>
+
+template <class T>
+class SortedTree :
+    public Tree<T> {
 public:
-    void add(T* element) override;
+    SortedTree();
+    string add(T* element) override;
+    string add(T& element) override;
     NWP<T>* find(T* compare_object);
-    void remove(bool (*predicate)(T*), Node<T>* (*normalization)(Node<T>*), bool recursively = false) override;
-private:
-    void removeImplementation(Node<T>* iterator, bool (*predicate)(T*), Node<T>* (*normalization)(Node<T>*), bool recursively);
+    void remove(function<bool(T*)> predicate, function<Node<T>* (Node<T>*)> normalization, bool recursively = false) override;
+    virtual List<T>* toList() override;
+    virtual void balanceHeight();
+protected:
+    void removeImplementation(Node<T>* iterator, function<bool(T*)> predicate, function<Node<T>* (Node<T>*)> normalization, bool recursively);
+    virtual void getAllHelper(Node<T>* current, List<T>*& list) override;
+    virtual void balanceHeightHelper(List<T>*& asList, int left, int right);
 public:
     ~SortedTree();
 };
+
+template<class T>
+SortedTree<T>::SortedTree() :Tree<T>() {
+
+}
+
+template <class T>
+string SortedTree<T>::add(T* element) {
+    NWP<T>* data = new NWP<T>();
+    std::string path("root:");
+
+    if (Tree<T>::root != nullptr) {
+        data = find(element);
+
+        if (*data->element->element < *element) {
+            data->element->right = new Node<T>(element, data->element);
+        }
+        else {
+            data->element->left = new Node<T>(element, data->element);
+        }
+
+        path = data->path;
+    }
+    else {
+        Tree<T>::root = new Node<T>(element);
+    }
+
+    delete data;
+    return(path);
+}
+
+template <class T>
+string SortedTree<T>::add(T& element) {
+    return add(new T(element));
+}
+
+template <class T>
+void SortedTree<T>::remove(function<bool(T*)> predicate, function<Node<T>* (Node<T>*)> normalization, bool recursively) {
+    removeImplementation(Tree<T>::root, predicate, normalization, recursively);
+}
+
+template<class T>
+List<T>* SortedTree<T>::toList()
+{
+    List<T>* result = new List<T>();
+    SortedTree<T>::getAllHelper(Tree<T>::root, result);
+    return result;
+}
+
+template<class T>
+void SortedTree<T>::balanceHeight()
+{
+    List<T>* asList = this->toList();
+    removeImplementation(Tree<T>::root, [](T* e) {return true; }, NULL, true);
+    Tree<T>::root = nullptr;
+    balanceHeightHelper(asList, 0, asList->Size() - 1);
+    delete asList;
+}
+
+template <class T>
+NWP<T>* SortedTree<T>::find(T* compare_object) {
+    NWP<T>* data = new NWP<T>();
+
+    if (Tree<T>::root != nullptr) {
+
+        data->path = "root:";
+
+        for (Tree<T>::current = Tree<T>::root; Tree<T>::current != nullptr;) {
+            Tree<T>::previous = Tree<T>::current;
+
+            if (*Tree<T>::previous->element == *compare_object) {
+                break;
+            }
+            else if (*Tree<T>::current->element < *compare_object) {
+                Tree<T>::current = Tree<T>::current->right;
+                data->path += "r,";
+            }
+            else {
+                Tree<T>::current = Tree<T>::current->left;
+                data->path += "l,";
+            }
+        }
+        data->element = Tree<T>::previous;
+
+        data->path.resize(data->path.length() - 1);
+
+    }
+    return(data);
+}
+
+template <class T>
+void SortedTree<T>::removeImplementation(Node<T>* iterator, function<bool(T*)> predicate, function<Node<T>* (Node<T>*)> normalization, bool recursively) {
+    if (iterator != nullptr) {
+        if (predicate(iterator->element)) {
+
+            if (recursively) {
+                removeImplementation(iterator->left, [](T* e) {return true; }, NULL, true);
+                removeImplementation(iterator->right, [](T* e) {return true; }, NULL, true);
+                delete iterator;
+                return;
+            }
+            else {
+                Tree<T>::previous = iterator;
+                iterator = normalization(iterator);
+                delete Tree<T>::previous;
+            }
+
+        }
+
+        removeImplementation(iterator->left, predicate, normalization, recursively);
+        removeImplementation(iterator->right, predicate, normalization, recursively);
+
+    }
+}
+
+template<class T>
+void SortedTree<T>::getAllHelper(Node<T>* current, List<T>*& list)
+{
+    if (current != nullptr) {
+        getAllHelper(current->left, list);
+        list->add(*current->element);
+        getAllHelper(current->right, list);
+    }
+}
+
+template<class T>
+void SortedTree<T>::balanceHeightHelper(List<T>*& asList, int left, int right)
+{
+    if (left <= right) {
+        int middle = (right + left) / 2;
+
+        this->add(*(*asList)[middle]);
+        balanceHeightHelper(asList, left, middle - 1);
+        balanceHeightHelper(asList, middle + 1, right);
+    }
+}
+
+template <class T>
+SortedTree<T>::~SortedTree() {
+    removeImplementation(Tree<T>::root, [](T* e) {return true; }, NULL, true);
+    Tree<T>::root = nullptr;
+}
+
+#pragma endregion
