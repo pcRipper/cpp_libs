f6111bfa9c0e4321a901a72a695c7c9d7881f068
pcRipper <maximuspro100@gmail.com>
2023-01-01 13:38:49 +0200

    added BigNums

---
+++ Headers/BigNum.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct numPair{
+public:
+	
+	int digit;
+	size_t power;
+	
+	numPair(int digit,size_t power);
+	
+	friend bool operator == (const numPair & left,const numPair & right);
+	friend ostream& operator<<(ostream& out,const numPair& obj);
+	~numPair();
+};
+
+class BigNum{
+	
+	bool sign;
+	vector<numPair> num;
+
+public:
+	
+	BigNum();
+	BigNum(long long number);
+	BigNum(string number,bool sign = true);
+//	BigNum(vector<numPair> dNumber);
+
+	void add(numPair pair);
+	void show();
+	BigNum operator*(const BigNum& obj);
+	
+	friend ostream& operator<<(ostream& out, const BigNum& obj);
+
+	~BigNum();
+	
+};
