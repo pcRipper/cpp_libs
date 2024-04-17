4a541aae056046b0ced13b79d163cc9cdc8169ec
pcRipper <maximuspro100@gmail.com>
2024-04-17 17:29:42 +0300

    mc

---
+++ Headers/Tree/Functors/Functors.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "../../includes.hpp"
+
+template <typename Type>
+Type* fromLevelOrderFunctor(const string& str) = delete;
+
+template <>
+int* fromLevelOrderFunctor(const string& str){
+    if(str.length() == 0 || !isdigit(str[0]))return nullptr;
+    return new int(stoi(str));
+}
---
+++ Source/Tree/Functors/Functors.cpp
@@ -0,0 +1,1 @@
+#include "../../../Headers/Tree/Functors/Functors.hpp"
