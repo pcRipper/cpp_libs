0c1c1641716c3f8e165c959f3e2819ebd0f5854c
pcRipper <maximuspro100@gmail.com>
2024-04-27 12:11:28 +0300

    remaked with base StaticArray base class, functionality keeped

--- Headers/StaticSized/StaticStack.hpp
+++ Headers/StaticSized/StaticStack.hpp
@@ -1,66 +1,62 @@
 #pragma once
-#include "../Iterators/ContinuousIterator.hpp"
+#include "StaticArray.hpp"
 
 /// @brief  Stack with predefined max size
 /// @tparam Type 
 /// @tparam Size sets max size of the stack
 /// @tparam Offset sets offset from start of the stack(0 index), indexing will be computed so : stack[index_required + offset]  
 template<int capacity, class Type = int, int offset = 0>
-class StaticStack{
+class StaticStack : private StaticArray<capacity, Type> {
 public:
-    using ValueType = Type;
-    using Iterator = ContinuousIterator<StaticStack<capacity, ValueType, offset>>;
-private:
-    Type stack[capacity];
-    int currentSize;
-
+    using BaseClassType = StaticArray<capacity, Type>;
+protected:
     static constexpr int MAX_SIZE = capacity - offset;
 public:
-    StaticStack(){
-        currentSize = 0;
-        memset(stack, 0, sizeof(ValueType) * capacity);
-    };
+    StaticStack():
+        StaticArray()
+    {};
 
     void push(Type element){
         if(currentSize >= MAX_SIZE)return;
-        stack[currentSize++ + offset] = element;
+        array[currentSize++ + offset] = element;
     }
 
-    Type top(){
+    void emplace(Type&& element){
+        if(currentSize >= MAX_SIZE)return;
+        array[currentSize++ + offset] = std::move(element);
+    }
+
+    Type& top(){
         return currentSize == 0
-            ? Type()
-            : stack[currentSize - 1 + offset]
+            ? BaseClassType::defaultValue
+            : array[currentSize - 1 + offset]
         ;
     }
 
     Type pop(){
-        if(currentSize == 0)return Type();
-        return currentSize == 0
-            ? Type()
-            : stack[--currentSize + offset]
-        ;
+        auto top = back();
+        currentSize -= 1;
+        return top;
     }
 
     int size(){
-        return currentSize; 
+        return BaseClassType::size();
     }
 
     void clear(){
-        currentSize = 0;
+        BaseClassType::clear();
     }
 
-    Type& operator[](int index) {
-        static Type defaultValue = Type();
-        if(index >= currentSize)return defaultValue;
-        return stack[index + offset];
+    virtual Type& operator[](int index){
+        return BaseClassType::operator[](index + offset);
     }
 
-    Iterator begin(){
-        return Iterator(stack + offset);
+    Iterator begin() override {
+        return Iterator(array + offset);
     }
 
-    Iterator end(){
-        return Iterator(stack + offset + currentSize);
+    Iterator end() override {
+        return Iterator(array + offset + currentSize);
     }
 
     ~StaticStack() = default;
